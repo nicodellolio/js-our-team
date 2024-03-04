bfe58142e71d8874bbd6f67b46f48b62b898e46f
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-04 14:44:57 +0100

    add milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,40 @@
 //file JS su cui lavorare:
 const team = [
     {
-      name: 'Wayne Barnett',
-      role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: './assets/img/wayne-barnett-founder-ceo.jpg',
     },
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: './assets/img/angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: './assets/img/walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: './assets/img/angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: './assets/img/scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: './assets/img/barbara-ramos-graphic-designer.jpg',
     },
-  ];
+];
+
+
+//creiamo un nodo della DOM
+const rowElement = document.querySelector('.row')
 
 //milestone 1: stampiamo le informazioni relative ad ogni membro in console
 //iniziamo utilizzando un ciclo for
@@ -39,9 +43,11 @@ for (let i = 0; i < team.length; i++) {
     const teamMember = team[i];
     // console.log(teamMember);
 
-    //che a sua volta conterrà un ciclo in
-    for (const key in teamMember) {
-        console.log(teamMember[key]);
-    }
+    //creiamo un markup da inserire nel DOM
+    const markup = `<div class="col"><div class="card p-2"><h3>${teamMember.name}</h3><aside>${teamMember.role}</div></div>`
+    console.log(markup);
+    rowElement.insertAdjacentHTML('beforeend', markup)
 }
+
+
 
